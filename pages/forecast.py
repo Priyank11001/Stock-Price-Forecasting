d58b5fd9e0a180c82d67f0609759bafb01633863
Priyank11001 <982005priyank@gmail.com>
2024-12-20 22:00:50 +0530

    Changes in forecast.py

--- pages/forecast.py
+++ pages/forecast.py
@@ -25,7 +25,7 @@ def run():
             stock_data['Date'] = pd.to_datetime(stock_data['Date'])
             st.session_state.count+=1
             
-        model = load_model("pages\stock_price_forecasting_model.keras")
+        model = load_model("pages/stock_price_forecasting_model.keras")
         data = stock_data['Close']
         scaled_data = scaler.fit_transform(np.array(data).reshape(-1,1))
         lookback = 30
